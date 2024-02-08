2b9d0047fd830a83de38889dc06e5014e46e6ec4
HEMANTHONAIR <thehemanthmohan@gmail.com>
2024-02-08 16:00:29 +0530

    searching

--- controllers/covidRouter.js
+++ controllers/covidRouter.js
@@ -15,5 +15,11 @@ router.get("/viewall",async(req,res)=>{
     let data=await covidModel.find()
     res.json(data)
 })
+router.post("/search",async(req,res)=>{
+    let input=req.body
+    let data=await covidModel.find(input)
+    res.json(data)
+})
+
 
 module.exports=router
